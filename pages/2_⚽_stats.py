41c0447386c71162c259011da1abd3e01e689172
divyashie <d.soopal@gmail.com>
2022-11-29 17:45:49 +0400

    working on stats page

--- pages/2_⚽_stats.py
+++ pages/2_⚽_stats.py
@@ -1,18 +1,33 @@
-import streamlit as st 
-import Home as h 
-from googlesearch import search 
+import streamlit as st
+import Home as h  
 import pandas as pd 
-import requests 
-from bs4 import BeautifulSoup
+import numpy as np
 
-SHEET_ID = '1GdZDJVy1RPSiwwyQcnMg6RiWzlRIGOslbia2hfRcHys'
-SHEET_NAME = 'Evaluate'
-url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+st.subheader("Bienvenue à la page des scores!")
+st.markdown("Voici les détails de la compilation des scores: ")
+st.markdown("<ul><li>Prédiction exacte du score = <b>3 points</b></ul></li>",unsafe_allow_html=True)
+st.markdown("<ul><li>Prédiction de l'équipe gagnante ou du match nul = <b>1 point</b></ul></li>",unsafe_allow_html=True)
+st.markdown("<ul><li>le défault = <b>0 point</b></ul></li>",unsafe_allow_html=True)
 
-# df = pd.read_csv(url,error_bad_lines=False)
-# print(df.head(100))
+def scoreLogic(): 
+    df = h.loadSpreadsheet('evaluate') 
+    df.columns = df.iloc[0]  #set first row of dataframe to column names 
+    df = df[1:] #set the data starting from second row 
+    conditions = [((df['score1'] == df['actual1']) & (df['score2'] == df['actual2'])), 
+                  (((df['score1'] > df['score2']) & (df['actual1'] > df['actual2'])) | ((df['score2'] > df['score1']) & (df['actual2'] > df['actual1']))), 
+                  (((df['score1'] == df['score2']) & (df['actual1'] == df['actual2'])))
+                  ]
+    choices = [3, 1, 1]
+    df['result'] = np.select(conditions, choices, default=0)
+    return df
+    
+def aggregateScores(df): 
+    df['name'] = df['name'].str.strip()
+    st.dataframe(df.groupby(['name']).sum()) 
+
 def main(): 
-    st.warning("Under Construction!")
+    df = scoreLogic()
+    aggregateScores(df)
 
 if __name__ == "__main__": 
     main()
